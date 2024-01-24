250f069d7db0b846311685d117481327cd3bde15
CabageMan <1iron.head1@gmail.com>
2024-01-24 19:13:39 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CongratulatorConsole;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nameConstant = " name";
+        Console.WriteLine("Hello!\nWhat is yout {0}?", nameConstant);
+        var userName = Console.ReadLine();
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {userName}, on {currentDate:dddd} at {currentDate:T}");
+        Console.Write($"{Environment.NewLine}Press any key to exit ...");
+        Console.ReadKey(false);
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# CongratulatorConsole
+
+## Functionality:
+- Shows birthdays list;
+- Shows todays and upcoming birthdays;
+- Adding, deleting and editing birthday records;
